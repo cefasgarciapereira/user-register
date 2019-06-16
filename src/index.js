1895a1aa697fc01ab3cfb72876048c23088c9d30
Cefas Garcia Pereira <cefasgarciapereira@gmail.com>
2019-06-16 16:07:34 -0300

    Rest implemented

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
- const express = require('express');
- const bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
- const app = express();
+const app = express();
 
- app.use(bodyParser.json());
- app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+ 
+require('./controllers/authController')(app);
 
- app.get('/',(req, res) => {
-    res.send("Hello, World");
- });
-
- app.listen(3000);
+app.listen(3000);
