6e1da6cc83f885f2ea9a8b5b36dcd720a0f9c75a
Cefas Garcia Pereira <cefasgarciapereira@gmail.com>
2019-06-16 16:23:11 -0300

    Hide password from response

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,7 +4,16 @@ const router = express.Router();
 
 router.post('/register', async (request, response) => {
     try{
+        const {email} = request.body;
+
+        if (await User.findOne({email})){
+            return response.send(400).send({error: 'User already exists.'});
+        }
+
         const user = await User.create(request.body);
+        
+        user.password = undefined;
+
         return response.send({user});
     }catch(err){
         return response.status(400).send({error: 'Registration failed.'});
